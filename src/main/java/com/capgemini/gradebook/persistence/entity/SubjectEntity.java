939eb105555b0a807f45be52273b1c3f57d3ad31
shpachynska <viktoriya.shpachynska@capgemini.com>
2023-02-03 10:19:29 +0100

    added entity properties

--- src/main/java/com/capgemini/gradebook/persistence/entity/SubjectEntity.java
+++ src/main/java/com/capgemini/gradebook/persistence/entity/SubjectEntity.java
@@ -22,7 +22,70 @@ public class SubjectEntity extends AbstractEntity {
   @ManyToOne(fetch = FetchType.LAZY)
   private TeacherEntity teacherEntity;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  private ClassEntity classEntity;
 
+  /**
+   * @return name
+   */
+  public String getName() {
 
+    return this.name;
+  }
 
+  /**
+   * @param name the new value.
+   */
+  public void setName(String name) {
+
+    this.name = name;
+  }
+
+  /**
+   * @return subjectType
+   */
+  public SubjectType getSubjectType() {
+
+    return this.subjectType;
+  }
+
+  /**
+   * @param subjectType the new value.
+   */
+  public void setSubjectType(SubjectType subjectType) {
+
+    this.subjectType = subjectType;
+  }
+
+  /**
+   * @return teacherEntity
+   */
+  public TeacherEntity getTeacherEntity() {
+
+    return this.teacherEntity;
+  }
+
+  /**
+   * @param teacherEntity the new value.
+   */
+  public void setTeacherEntity(TeacherEntity teacherEntity) {
+
+    this.teacherEntity = teacherEntity;
+  }
+
+  /**
+   * @return classEntity
+   */
+  public ClassEntity getClassEntity() {
+
+    return this.classEntity;
+  }
+
+  /**
+   * @param classEntity the new value.
+   */
+  public void setClassEntity(ClassEntity classEntity) {
+
+    this.classEntity = classEntity;
+  }
 }
--- src/main/java/com/capgemini/gradebook/persistence/entity/TeacherEntity.java
+++ src/main/java/com/capgemini/gradebook/persistence/entity/TeacherEntity.java
@@ -14,14 +14,29 @@ public class TeacherEntity extends AbstractEntity {
   private String firstName;
   private String lastName;
 
-
-  //TODO IMPLEMENT: after creating subjectEntity and other subject classes uncomment the lines below, making necessary
+   //TODO IMPLEMENT: after creating subjectEntity and other subject classes uncomment the lines below, making necessary
   // adjustments; then generate getters and setters and fix the mappers
   @OneToMany(mappedBy = "teacherEntity", fetch = FetchType.LAZY)
   private List<SubjectEntity> subjectEntityList;
 
 
   //TODO IMPLEMENT: create @OneToMany with mappedBy to subjects after you create a base model
+
+  /**
+   * @return subjectEntityList
+   */
+  public List<SubjectEntity> getSubjectEntityList() {
+
+    return this.subjectEntityList;
+  }
+
+  /**
+   * @param subjectEntityList the new value.
+   */
+  public void setSubjectEntityList(List<SubjectEntity> subjectEntityList) {
+
+    this.subjectEntityList = subjectEntityList;
+  }
 
   public String getFirstName() {
 
